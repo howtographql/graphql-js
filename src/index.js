a0cf58c161f9cef1af2b0494038b584d958fddd4
Maira Bello <maira.bello@vtex.com.br>
2017-06-26 12:25:45 -0300

    Connect to Mongo DB

--- src/index.js
+++ src/index.js
@@ -8,14 +8,23 @@ const bodyParser = require('body-parser');
 const {graphqlExpress, graphiqlExpress} = require('graphql-server-express');
 
 const schema = require('./schema');
+const connectMongo = require('./mongo-connector')
 
-var app = express();
-app.use('/graphql', bodyParser.json(), graphqlExpress({schema}));
-app.use('/graphiql', graphiqlExpress({
-  endpointURL: '/graphql',
-}));
+const start = async () => {
+  const mongo = await connectMongo()
+  var app = express();
+  app.use('/graphql', bodyParser.json(), graphqlExpress({
+    context: {mongo}, // This context object is passed to all resolvers.
+    schema
+  }));
+  app.use('/graphiql', graphiqlExpress({
+    endpointURL: '/graphql',
+  }));
 
-const PORT = 3000
-app.listen(PORT, () => {
-  console.log(`Hackernews GraphQL server running on port ${PORT}.`)
-});
+  const PORT = 3000
+  app.listen(PORT, () => {
+    console.log(`Hackernews GraphQL server running on port ${PORT}.`)
+  });
+}
+
+start()
---
+++ src/mongo-connector.js
@@ -0,0 +1,18 @@
+const {MongoClient} = require('mongodb');
+
+/**
+ * 1. Specify the url for connecting to the desired MongoDB instance.
+ * This is the default url usually available, but feel free to replace
+ * it with your own if different.
+ */
+const MONGO_URL = 'mongodb://localhost:27017/hackernews';
+
+/**
+ * 2. Export a function that connects to the db and returns the collections
+ * your resolvers will use.
+ */
+module.exports = async () => {
+  // Connecting to MongoDB is an async operation, so we need to wait here.
+  const db = await MongoClient.connect(MONGO_URL);
+  return {Links: db.collection('links')};
+}
