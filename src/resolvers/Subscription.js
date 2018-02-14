5ac974a98fd7174666df1b8e3d2f930db8340157
Nikolas <nikolas.burk@gmail.com>
2018-02-14 19:06:45 +0100

    Update Subscription.js

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -1,7 +1,7 @@
 const newLink = {
   subscribe: (parent, args, ctx, info) => {
     return ctx.db.subscription.link(
-      // https://github.com/graphcool/prisma/issues/1734
+      // see https://github.com/howtographql/graphql-js/issues/14
       // { where: { mutation_in: ['CREATED'] } },
       { },
       info,
@@ -12,7 +12,7 @@ const newLink = {
 const newVote = {
   subscribe: (parent, args, ctx, info) => {
     return ctx.db.subscription.vote(
-      // https://github.com/graphcool/prisma/issues/1734
+      // see https://github.com/howtographql/graphql-js/issues/14
       // { where: { mutation_in: ['CREATED'] } },
       { },
       info,
