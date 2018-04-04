9934b83f2b9a55c2db8dd59ccdad481575c2b615
Nikolas Burk <nikolas.burk@gmail.com>
2018-04-04 09:17:35 +0200

    update to new tutorial flow

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -1,23 +1,27 @@
+function newLinkSubscribe (parent, args, context, info) {
+  return context.db.subscription.link(
+    // https://github.com/graphcool/prisma/issues/1734
+    // { where: { mutation_in: ['CREATED'] } },
+    { },
+    info,
+  )
+}
+
 const newLink = {
-  subscribe: (parent, args, ctx, info) => {
-    return ctx.db.subscription.link(
-      // https://github.com/graphcool/prisma/issues/1734
-      // { where: { mutation_in: ['CREATED'] } },
-      { },
-      info,
-    )
-  },
+  subscribe: newLinkSubscribe
+}
+
+function newVoteSubscribe (parent, args, context, info) {
+  return context.db.subscription.vote(
+    // https://github.com/graphcool/prisma/issues/1734
+    // { where: { mutation_in: ['CREATED'] } },
+    { },
+    info,
+  )
 }
 
 const newVote = {
-  subscribe: (parent, args, ctx, info) => {
-    return ctx.db.subscription.vote(
-      // https://github.com/graphcool/prisma/issues/1734
-      // { where: { mutation_in: ['CREATED'] } },
-      { },
-      info,
-    )
-  },
+  subscribe: newVoteSubscribe
 }
 
 module.exports = {
