a2803baa0c990853fba2e40ce4e2dd77e4bdc1ce
Nikolas Burk <nikolas.burk@gmail.com>
2018-12-17 12:15:01 +0100

    convert app to prisma-client

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,33 +1,3 @@
-// async function feed(parent, args, context) {
-//   const where = args.filter
-//     ? {
-//         OR: [
-//           { url_contains: args.filter },
-//           { description_contains: args.filter },
-//         ],
-//       }
-//     : {}
-
-//   const queriedLinkes = await context.prisma.links(
-//     { where, skip: args.skip, first: args.first, orderBy: args.orderBy },
-//     `{ id }`,
-//   )
-
-//   const countSelectionSet = `
-//     {
-//       aggregate {
-//         count
-//       }
-//     }
-//   `
-//   const linksConnection = await context.prisma.linksConnection({}, countSelectionSet)
-
-//   return {
-//     count: linksConnection.aggregate.count,
-//     linkIds: queriedLinkes.map(link => link.id),
-//   }
-// }
-
 async function feed(parent, args, context) {
   const count = await context.prisma
     .linksConnection({
